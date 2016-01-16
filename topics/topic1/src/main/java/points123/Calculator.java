c0e07b765faef45b45a20ca6e2662188a9111c1c
jmabate <juan.abate@gmail.com>
2016-01-16 20:15:43 -0300

    topic 1 practice

---
+++ topics/topic1/src/main/java/points123/Calculator.java
@@ -0,0 +1,15 @@
+package points123;
+
+public class Calculator {
+	
+	public Calculator(){
+	}
+	
+	public int Sum(int first, int second){
+		return first+second;
+	}
+	
+	public int Substract(int first, int second){
+		return first-second;
+	}
+}
