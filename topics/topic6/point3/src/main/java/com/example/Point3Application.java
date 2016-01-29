042202da7e6e1113aaf0f50bc334631fecd66b87
jmabate <juan.abate@gmail.com>
2016-01-29 17:55:08 -0300

    t7 p3

---
+++ topics/topic6/point3/src/main/java/com/example/Point3Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Point3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Point3Application.class, args);
+	}
+}
